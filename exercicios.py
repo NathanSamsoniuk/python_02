c2e7d25317d95363818d120987f92964eb1f7b62
Nathan Samsoniuk <nathan.samsoniuk@outlook.com>
2024-11-19 23:15:12 -0300

    adicionado exercício 03

--- exercicios.py
+++ exercicios.py
@@ -14,6 +14,12 @@ remainder = number % 5
 print(f"The remainder is: {remainder}")
 
 # 3. Desenvolva um programa que multiplique dois números fornecidos pelo usuário e mostre o resultado.
+
+number_01 = int(input("Please enter first value: "))
+number_02 = int(input("Please enter second value: "))
+result = (number_01 * number_02)
+print (f"The result is: {result}")
+
 # 4. Faça um programa que peça dois números inteiros e imprima a divisão inteira do primeiro pelo segundo.
 # 5. Escreva um programa que calcule o quadrado de um número fornecido pelo usuário.
 
